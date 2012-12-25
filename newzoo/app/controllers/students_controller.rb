c5b383857343a0397e2f5f0a10fc7f8e4fcfdc39
SOA2012-101078506 <linkin3254317@gmail.com>
2012-12-25 20:15:52 +0800

    edit and destroy done

--- newzoo/app/controllers/students_controller.rb
+++ newzoo/app/controllers/students_controller.rb
@@ -3,12 +3,34 @@
    def index
      @students = Student.all
    end
-   
+   def update
+     @student = Student.find params[:id]
+     @student.update_attributes!(params[:student])
+     respond_to do |client_wants|
+     client_wants.html {  redirect_to student_path(@student)  } # as before
+     client_wants.xml  {  render :xml => @student.to_xml    }
+     end
+   end
    def new
 
    end
+   def destroy
+     @student= Student.find(params[:id])
+     @student.destroy
+     flash[:notice] = "Student '#{@student.title}' deleted."
+     redirect_to students_path
+   end
 
-   
+   def edit
+     @student = Student.find params[:id]
+   end
+
+#   def update
+#     @student = Student.find params[:id]
+#     @student.update_attributes!(params[:student])
+#     flash[:notice] = "#{@student.title} was successfully updated."
+#     redirect_to student_path(@student)
+#   end
    def create
      @student = Student.create!(params[:student])
      redirect_to students_path
